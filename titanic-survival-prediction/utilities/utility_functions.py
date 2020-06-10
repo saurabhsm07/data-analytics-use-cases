57a6db25de38ebdd706cd4beb1bc97bf1a3f3116
Saurabh Mahambrey <mahambrey.saurabh07@gmail.com>
2020-06-10 23:49:43 +0530

    cleaned references and created a utilities module

--- titanic-survival-prediction/utilities/utility_functions.py
+++ titanic-survival-prediction/utilities/utility_functions.py
@@ -5,42 +5,47 @@ Created on Wed Oct 16 09:16:21 2019
 @author: saurabh_mahambrey
 """
 
-#required libraries:
-import pandas as pd;    
+# required libraries:
+import pandas as pd;
 import numpy as np;
 
-#importing training and test data into pandas dataframe
+
+# importing training and test data into pandas dataframe
 def import_data(path):
     return pd.read_csv(path)
 
 
-
-#utility functions
+# utility functions
 def print_info(df):
     return df.info()
+
 
 def print_description(df):
     return df.describe()
 
+
 def print_head(df, count=5):
     return df.head(count)
+
 
 # function returns a dictionary of percentage of null values in a dataframe
 def get_null_percent(df):
     null_percent_dict = {}
-    
+
     for col in df.columns:
-        null_percent_dict[col] = df[df[col].isnull() == True].loc[:,'Survived'].count()/ df.shape[0] *100
-    
+        null_percent_dict[col] = df[df[col].isnull() == True].loc[:, 'Survived'].count() / df.shape[0] * 100
+
     return null_percent_dict
+
 
 # get list of attributes with atleast 1 cell having a null value
 def get_attributes_with_nulls(df):
     df_nulls = pd.Series(df.isna().any())
     return df_nulls[df_nulls == True].index.tolist()
 
+
 def get_attributes_with_nulls_inorder(df):
-    return    (df.isnull()
-                 .sum(axis = 0)
-                 .loc[df.isnull().sum() > 0]
-                 .sort_values( ascending = True))
+    return (df.isnull()
+            .sum(axis=0)
+            .loc[df.isnull().sum() > 0]
+            .sort_values(ascending=True))
---
+++ titanic-survival-prediction/utilities/visualizations.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 16 09:42:09 2019
+
+@author: saurabh_mahambrey
+"""
+
+
+### Visualizations :
+
+"""
+Heatmap : correlation matrix visualization :
+          plot a heatmap of correlation matrix of attribute variables
+"""
+
+
+def plot_correlation_heatmap(correlation_data):
+    #    %matplotlib inline
+    import matplotlib.pyplot as plt
+    fig = plt.figure(figsize=(9, 6))
+    import seaborn as sns
+    return sns.heatmap(correlation_data, annot=True)
+
+
+"""
+PCA : 2 variable scatter plot visualization
+# <font color= 'red'>  IN Progress : create 2 sub plots 1: 2 variable scatter using pca 
+    and 2: 2 variable scatter based on correlation </font>
+"""
+
+
+def plot_pca_scatterplot(train, test):
+    #    %matplotlib inline
+    import matplotlib.pyplot as plt
+    plt.scatter(train[:, 0], train[:, 1], color='green', marker='X')
+    plt.scatter(test[:, 0], test[:, 1], color='red', marker='X')
